373004edf47810841bce8a002707a73d1d62ce15
Pokour <pokour@gmail.com>
2020-04-17 17:57:46 +0530

    updates on promise

--- promise.js
+++ promise.js
@@ -1,6 +1,12 @@
 const p = new Promise((resolve, reject) => {
     // Kick off some async work
     // ...
-    // resolve(1)
     // reject(err)
+    setTimeout( ()=> {
+        resolve(1);
+    },2000);
+});
+
+p.then(result => {
+    console.log(`the result is ${result}`);
 });
